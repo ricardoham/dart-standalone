060d41c17b3b0aa70140e88e521d1450f545cced
Ricardo Manoel <rdomanoel@gmail.com>
2020-01-13 23:41:14 -0300

    new requests functions

--- api/lib/controller/products_controller.dart
+++ api/lib/controller/products_controller.dart
@@ -1,24 +1,46 @@
 import 'package:aqueduct/aqueduct.dart';
 
 class ProductsController extends ResourceController {
-  final _products = [
+  List products = [
     {'id': 1, 'name': 'Television'},
     {'id': 2, 'name': 'Table'},
     {'id': 3, 'name': 'PC'},
   ];
 
+  // final List<Products> produc
   @Operation.get()
   Future<Response> getAllProducts() async {
-    return Response.ok(_products);
+    return Response.ok(products);
   }
 
   @Operation.get('id')
   Future<Response> getProductById(@Bind.path('id') int id) async {
       // final id = int.parse(request.path.variables['id']); // Deprecated
-      final product = _products.firstWhere((p) => p['id'] == id, orElse: () => null);
+      final product = products.firstWhere((p) => p['id'] == id, orElse: () => null);
       if(product == null) {
         return Response.notFound();
       }
-    return Response.ok(_products);
+    return Response.ok(products);
   }
+
+  @Operation.post()
+  Future<Response> createProduct(@Bind.body() product) async {
+    products.add(product);
+    return Response.ok(products);
+  }
+
+  @Operation.put()
+  Future<Response> changeProduct(@Bind.body() product) async {
+    products.removeAt(products.indexWhere((r) => r.id == product.id));
+    products.add(product);
+    return Response.ok(products);
+  }
+
+  @Operation.delete('id')
+  Future<Response> deleteProduct() async {
+    final id = int.parse(request.path.variables['id']);
+    products.removeAt(products.indexWhere((p) => p.id == id));
+    return Response.ok(products);
+  }
+
 }
